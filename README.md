91e7958bce4d9da146f84b8ed497af02106ffb77
Faith W <faith.welton@otekha.com>
2025-06-12 14:02:48 -0400

    Change from first to third person view, new player controller, new camera controller

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Following Tutorial at https://www.gdquest.com/library/first_3d_game_godot4_arena_fps/ to learn how to use Godot and make a basic game
+Starting point: https://www.gdquest.com/library/first_3d_game_godot4_arena_fps/ to learn how to use Godot and make a basic game
+Expanding/Changing:
+	- Changing from first person to third person
+		- Split Camera and Player into separate scenes
+		- Setup Player Controller
+		- Setup Camera Controller
+		- Removed all but one platform from world environment, made that platform bigger (temp)
+		
--- player/player.gd
+++ player/player.gd
@@ -1,52 +1,54 @@
 extends CharacterBody3D
 
-func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+@export var player_speed: float = 5.0
+@export var rotation_speed: float = 10.0
+@export var jump_height: float = 1.0
+@export var gravity_value: float = -9.81
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		rotation_degrees.y -= event.relative.x * 0.5
-		%Camera3D.rotation_degrees.x -= event.relative.y * 0.2
-		%Camera3D.rotation_degrees.x = clamp(
-			%Camera3D.rotation_degrees.x, -60.0, 60.0
-		)
-	elif event.is_action_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+@onready var follow_camera: Camera3D = get_node("../CameraController/Camera3D")
 
+func _ready():
+	# Ensure we're using the default gravity if not set
+	if not is_on_floor():
+		velocity.y = gravity_value
 
-func _physics_process(delta: float) -> void:
-	const SPEED = 5.5
+func _physics_process(delta):
+	movement(delta)
 
-	var input_direction_2D = Input.get_vector(
-		"move_left", "move_right", "move_forward", "move_back"
-	)
-	var input_direction_3D = Vector3(
-		input_direction_2D.x, 0, input_direction_2D.y
-	)
-	var direction = transform.basis * input_direction_3D
+func movement(delta):
+	# Handle gravity
+	if not is_on_floor():
+		velocity.y += gravity_value * delta
+	else:
+		# Reset vertical velocity when grounded
+		if velocity.y < 0:
+			velocity.y = 0
 
-	velocity.x = direction.x * SPEED
-	velocity.z = direction.z * SPEED
+	# Get input
+	var horizontal_input = Input.get_axis("move_left", "move_right")
+	var vertical_input = Input.get_axis("move_forward", "move_back")
 	
-	velocity.y -= 20.0 * delta
+	# Calculate movement direction relative to camera
+	var camera_transform = follow_camera.global_transform
+	var camera_basis = camera_transform.basis
 	
+	# Remove Y rotation from camera basis for movement calculation
+	var movement_input = camera_basis * Vector3(horizontal_input, 0, vertical_input)
+	movement_input.y = 0  # Ensure we don't move vertically based on camera angle
+	var movement_direction = movement_input.normalized()
+	
+	# Apply horizontal movement
+	velocity.x = movement_direction.x * player_speed
+	velocity.z = movement_direction.z * player_speed
+	
+	# Handle rotation
+	if movement_direction != Vector3.ZERO:
+		var desired_rotation = transform.looking_at(global_position + movement_direction, Vector3.UP)
+		transform.basis = transform.basis.slerp(desired_rotation.basis, rotation_speed * delta)
+	
+	# Handle jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = 10.0
-	elif Input.is_action_just_released("jump") and velocity.y > 0.0:
-		velocity.y = 0.0
-
+		velocity.y = sqrt(jump_height * -3.0 * gravity_value)
+	
+	# Move the character
 	move_and_slide()
-	
-	if Input.is_action_pressed("shoot") and %Timer.is_stopped():
-		shoot_bullet()
-
-func shoot_bullet():
-	const BULLET_3D = preload("res://player/bullet_3d.tscn")
-	var new_bullet = BULLET_3D.instantiate()
-	%Marker3D.add_child(new_bullet)
-	
-	new_bullet.global_transform = %Marker3D.global_transform
-	
-	%Timer.start()
-	
-	%AudioStreamPlayer.play()
